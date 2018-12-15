49b5365411311cc4ee58e3dacfaffcc08991120e
Supawat <supawattt@gmail.com>
2018-12-16 00:04:35 +0100

    able to join the group but has problem with multer field, idk why

--- src/server.js
+++ src/server.js
@@ -16,13 +16,10 @@ var storage = multer.diskStorage({
         cb(null,"./images")
     },
     filename: (req,file,cb)=>{
-        cb(null,
-            Math.random().toString(14).substring(2)+ '-'+Date.now()+".png"
-        )
+        cb(null,Date.now()+'-'+Math.random().toString(14).substring(2)+".png");
     } 
-})
-var upload = multer({storage});
-
+});
+var upload = multer({storage}).single("img");
 
 api.use(bodyParser.urlencoded({extended:true}));
 api.use(bodyParser.json());
@@ -37,21 +34,21 @@ api.get('/image/:id', (req,res)=>{
     res.sendFile(path.join(__dirname,"../images/"+imgurl));
 })
 
-api.post('/upload',upload.single('image'),(req,res)=>{
+api.post('/upload',upload,(req,res)=>{
     if(!req.file){
         console.log("No file!");
         return res.status(500).send({error:"no file"});
     }
+    console.log('done upload');
     return res.status(200).send({success: "done", url: req.file.filename});
 
 });
 
-
 api.post('/api/room/', (req,res)=>{
+    //console.log(req);
     var ikey = req.body.key;
     //console.log(ikey);
     Room.findOne({"roomkey":ikey}).then((data)=>{
-        if(!res)res.status(404).send('this key is invalid');
         res.status(200).send(data);
     }).catch((err)=>{
         res.status(404).send('error');
@@ -70,11 +67,19 @@ api.post('/api/create', (req,res)=>{
     }).catch(err=>res.status(500).send(err));
 });
 api.patch('/api/join',(req,res)=>{
-    var roomkey = req.body.key;
-    Room.findOneAndUpdate({key}).then((room)=>{
-        room.users.push({email:req.body.email,name:req.body.name});
-        
-    }).catch(err=>res.status(500).send(err));
+    console.log('trying to jon');
+    var santa = {
+        name: req.body.name,
+        email: req.body.email,
+        image: req.body.image
+    }
+    Room.findOneAndUpdate({roomkey:req.body.key},{$push:{users:santa}},(err,suc)=>{
+        if(err){
+            res.status(404).send();
+        }else{
+            res.status(200).send();
+        }
+    });
 })
 api.listen(process.env.PORT, ()=>{
     console.log(`API is now running on port ${process.env.PORT}`)
