e9dc61f60aaf20c1666d656045f1ccfeea78146f
Supawat Tamsri <supawattt@gmail.com>
2020-04-19 12:14:35 +0200

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://therealsanta:santasantasanta2018@ds125352.mlab.com:25352/heroku_8hxp33rt';
+  process.env.MONGODB_URI = '';
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'none';
